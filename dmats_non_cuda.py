b45c9fd476a997b74d0f133e72e3b3fa9321437b
Josiah <josiah@Josiahs-MacBook-Air.local>
2022-07-19 16:10:56 -0400

    added dmats_non_cuda.py

---
+++ dmats_non_cuda.py
@@ -0,0 +1,19 @@
+from CERGenerator import *
+import sys
+
+
+nshots = 200
+p = float(sys.argv[1])
+outfile = sys.argv[3]
+q = float(sys.argv[2])
+
+nvertices = 12
+ngraphs = 100
+
+metrics = [edgeCountDistance, disagreementCount, specDistance, minDistanceCUDA, meanDistanceCUDA, doublyStochasticMatrixDistance]
+metricNames = {minDistanceCUDA: "minDistanceCUDA", meanDistanceCUDA: "meanDistanceCUDA", specDistance: "specDistance", edgeCountDistance: "edgeCountDistance", disagreementCount: "disagreementCount", doublyStochasticMatrixDistance: "doublyStochasticMatrixDistance"}
+
+for m in metrics:
+    mname = metricNames[m]
+    print(f"doing metric = {mname}")
+    chains_to_dmats_json_partial('data.json',m,mname,nvertices,p,q)
